eae8928907e49099a9199a7bc41886250608e0c8
Brian Azevedo <100107574+trapwig@users.noreply.github.com>
2022-05-30 11:12:27 -0300

    Add files via upload

--- polimorfismo/src/Funcao.cpp
+++ polimorfismo/src/Funcao.cpp
@@ -16,3 +16,65 @@ Funcao::~Funcao() {
 	// TODO Auto-generated destructor stub
 }
 
+Constante::Constante() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Constante::~Constante() {
+	// TODO Auto-generated destructor stub
+}
+
+Escalar::Escalar() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Escalar::~Escalar() {
+	// TODO Auto-generated destructor stub
+}
+
+Potencial::Potencial() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Potencial::~Potencial() {
+	// TODO Auto-generated destructor stub
+}
+
+Exponencial::Exponencial() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Exponencial::~Exponencial() {
+	// TODO Auto-generated destructor stub
+}
+
+Seno::Seno() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Seno::~Seno() {
+	// TODO Auto-generated destructor stub
+}
+
+Cosseno::Cosseno() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Cosseno::~Cosseno() {
+	// TODO Auto-generated destructor stub
+}
+
+FuncaoAgregada::FuncaoAgregada() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FuncaoAgregada::~FuncaoAgregada() {
+	// TODO Auto-generated destructor stub
+}
